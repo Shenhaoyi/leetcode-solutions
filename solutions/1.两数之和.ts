01d606b2f16ead97d2b5eeabcbc1f3d06af20ef5
Shenhaoyi <shenhaoyi04@163.com>
2023-09-20 15:36:14 +0800

    1.twoSum

---
+++ solutions/1.两数之和.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  let result: number[] = [];
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const current = nums[i];
+    const diff = target - current;
+    if (map.has(diff)) {
+      result = [i, map.get(diff)];
+    } else {
+      map.set(current, i);
+    }
+  }
+  return result;
+}
+// @lc code=end
