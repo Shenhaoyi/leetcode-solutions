b2721cc059ca9f983a62438e192e4f813934e47f
Shenhaoyi <shenhaoyi04@163.com>
2023-09-28 22:51:52 +0800

    160.相交链表

---
+++ solutions/160.相交链表.ts
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=typescript
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+  if (headA === null || headB === null) return null;
+  let result: ListNode | null = null;
+  // 方法1：2层for循环
+  // 方法2：哈希表
+  /*
+    方法3：
+    l1 = a + c
+    l2 = b + c
+    那么两个链表拼接
+    l1+l2 = a+c+b+c
+    l2+l1 = b+c+a+c
+    则遍历的时候，必在c处碰上
+  */
+  let currA: ListNode | null = headA;
+  let currB: ListNode | null = headB;
+  while (true) {
+    if (currA === currB) {
+      // 如果同时为null肯定表示没有交点
+      result = currA;
+      break;
+    }
+    if (currA === null) {
+      currA = headB;
+    } else {
+      currA = currA.next;
+    }
+    if (currB === null) {
+      currB = headA;
+    } else {
+      currB = currB.next;
+    }
+  }
+  return result;
+}
+// @lc code=end
