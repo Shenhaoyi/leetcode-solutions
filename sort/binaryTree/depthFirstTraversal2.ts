7ccb8eb7cf29fc596e748d69a54def0f94beb485
Shenhaoyi <shenhaoyi04@163.com>
2023-09-29 19:58:01 +0800

    使用迭代实现遍历

--- sort/binaryTree/depthFirstTraversal2.ts
+++ sort/binaryTree/depthFirstTraversal2.ts
@@ -1,11 +1,17 @@
-// 使用迭代
+// 使用迭代（不太容易记住，尽量使用递归）
 // 访问当前节点、左子节点、右子节点的顺序
 
 /* 先序遍历 */
 export function preOrder2(root: TreeNode | null): number[] {
   if (root === null) return [];
   const result: number[] = [];
-  // TODO:
+  const stack = [root];
+  while (stack.length) {
+    const current = stack.pop() as TreeNode;
+    result.push(current.val);
+    current.right && stack.push(current.right);
+    current.left && stack.push(current.left);
+  }
   return result;
 }
 
@@ -13,14 +19,30 @@ export function preOrder2(root: TreeNode | null): number[] {
 export function inOrder2(root: TreeNode | null): number[] {
   if (root === null) return [];
   const result: number[] = [];
-  // TODO:
+  const stack = [];
+  let current: TreeNode | null = root;
+  while (stack.length || current) {
+    while (current) {
+      stack.push(current);
+      current = current.left;
+    }
+    current = stack.pop() as TreeNode;
+    result.push(current.val);
+    current = current.right;
+  }
   return result;
 }
 
-/* 后序遍历 */
+/* 后序遍历: 反先序+reverse */
 export function postOrder2(root: TreeNode | null): number[] {
   if (root === null) return [];
   const result: number[] = [];
-  // TODO:
-  return result;
+  const stack = [root];
+  while (stack.length) {
+    const current = stack.pop() as TreeNode;
+    result.push(current.val);
+    current.left && stack.push(current.left);
+    current.right && stack.push(current.right);
+  }
+  return result.reverse();
 }
