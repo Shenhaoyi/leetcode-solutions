7052d43cd2da82701828cf09d397b0ee1bb7b9d8
Shenhaoyi <shenhaoyi04@163.com>
2023-09-29 18:11:26 +0800

    层序遍历

--- global.d.ts
+++ global.d.ts
@@ -3,3 +3,10 @@ declare class ListNode {
   next: ListNode | null;
   constructor(val?: number, next?: ListNode | null);
 }
+
+declare class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null);
+}
---
+++ sort/binaryTree/levelOrderTraversal.ts
@@ -0,0 +1,13 @@
+/* 层序遍历 */
+function levelOrder(root: TreeNode | null): number[] {
+  if (root === null) return [];
+  const result: number[] = [];
+  const queue = [root];
+  while (queue.length > 0) {
+    const current = queue.shift() as TreeNode;
+    result.push(current.val);
+    current.left && queue.push(current.left);
+    current.right && queue.push(current.right);
+  }
+  return result;
+}
