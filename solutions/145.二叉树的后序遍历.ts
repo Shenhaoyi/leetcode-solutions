86edffab10592f5d26e5e542e2dcdcf6866bae39
Shenhaoyi <shenhaoyi04@163.com>
2023-09-29 21:58:17 +0800

    145.二叉树的后序遍历

---
+++ solutions/145.二叉树的后序遍历.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=typescript
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+  if (!root) return [];
+  const result: number[] = [];
+  const help = (node: TreeNode) => {
+    node.left && help(node.left);
+    node.right && help(node.right);
+    result.push(node.val);
+  };
+  help(root);
+  return result;
+}
+// @lc code=end
