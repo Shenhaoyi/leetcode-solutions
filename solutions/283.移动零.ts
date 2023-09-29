6343a9455c8e8c725bf290b61ac4862cfb95fa7a
Shenhaoyi <shenhaoyi04@163.com>
2023-09-29 20:46:53 +0800

    注释

--- solutions/283.移动零.ts
+++ solutions/283.移动零.ts
@@ -11,19 +11,19 @@
 function moveZeroes(nums: number[]): void {
   // 解法1：双重循环
   // 0时，找到后面第一个非0的进行调换就行
-  for (let i = 0; i < nums.length; i++) {
-    const current = nums[i];
-    if (current === 0) {
-      for (let j = i + 1; j < nums.length; j++) {
-        const childCurrent = nums[j];
-        if (childCurrent !== 0) {
-          nums[i] = childCurrent;
-          nums[j] = 0;
-          break;
-        }
-      }
-    }
-  }
+  // for (let i = 0; i < nums.length; i++) {
+  //   const current = nums[i];
+  //   if (current === 0) {
+  //     for (let j = i + 1; j < nums.length; j++) {
+  //       const childCurrent = nums[j];
+  //       if (childCurrent !== 0) {
+  //         nums[i] = childCurrent;
+  //         nums[j] = 0;
+  //         break;
+  //       }
+  //     }
+  //   }
+  // }
   // 解法2：快慢指针
   // 遇到非0的时候，慢指针++，遇到0的时候不+，所以慢指针最终一定指向第一个遇到的0
   // 保证两个指针中间的数字都是0
