467c054820bc27f637d99577261be1a367d46265
Shenhaoyi <shenhaoyi04@163.com>
2023-10-03 16:59:39 +0800

    代码优化

--- solutions/669.修剪二叉搜索树.ts
+++ solutions/669.修剪二叉搜索树.ts
@@ -23,8 +23,14 @@ function trimBST(root: TreeNode | null, low: number, high: number): TreeNode | n
   // 递归
   if (!root) return root;
   // 情况1：当前节点不在区间内
-  if (root.val > high) return trimBST(root.left, low, high);
-  if (root.val < low) return trimBST(root.right, low, high);
+  if (root.val > high) {
+    root.right = null; // 释放内存
+    return trimBST(root.left, low, high);
+  }
+  if (root.val < low) {
+    root.left = null;
+    return trimBST(root.right, low, high);
+  }
   // 情况2：当前节点在区间内
   root.left = trimBST(root.left, low, high);
   root.right = trimBST(root.right, low, high);
