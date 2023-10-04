de37e39616fab72a05c5673d01ae35a6b8dfa4c7
Shenhaoyi <shenhaoyi04@163.com>
2023-10-04 19:42:03 +0800

    优化代码

--- src/sort/heapSort.test.ts
+++ src/sort/heapSort.test.ts
@@ -2,12 +2,12 @@ import { heapSort, heapSort2 } from './heapSort';
 
 test('heap sort', () => {
   const nums = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234];
-  const nums1 = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234];
-  expect(heapSort(nums)).toEqual(nums1.sort((a, b) => a - b));
+  const target = [...nums].sort((a, b) => a - b);
+  expect(heapSort(nums)).toEqual(target);
 });
 
 test('heap sort 2', () => {
   const nums = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234];
-  const nums1 = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234];
-  expect(heapSort2(nums)).toEqual(nums1.sort((a, b) => a - b));
+  const target = [...nums].sort((a, b) => a - b);
+  expect(heapSort2(nums)).toEqual(target);
 });
