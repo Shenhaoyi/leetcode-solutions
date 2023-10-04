13cb4edfe71834ff9f83274d90f23dc856adec92
Shenhaoyi <shenhaoyi04@163.com>
2023-10-04 16:43:30 +0800

    修正测试用例

--- src/sort/quickSort.test.ts
+++ src/sort/quickSort.test.ts
@@ -8,14 +8,14 @@ test('partition', () => {
 
 test('quick sort 1', () => {
   const nums = [5, 4, 3, 2, 1];
-  const target = [5, 4, 3, 2, 1].sort((a, b) => a - b);
+  const target = [...nums].sort((a, b) => a - b);
   quickSort(nums);
   expect(nums).toEqual(target);
 });
 
 test('quick sort 2', () => {
   const nums = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234];
-  const target = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234].sort((a, b) => a - b);
+  const target = [...nums].sort((a, b) => a - b);
   quickSort(nums);
   expect(nums).toEqual(target);
 });
--- src/sort/quickSort2.test.ts
+++ src/sort/quickSort2.test.ts
@@ -2,10 +2,12 @@ import { quickSort2 } from './quickSort2';
 
 test('quick sort2 1', () => {
   const nums = [5, 4, 3, 2, 1];
-  expect(quickSort2(nums)).toEqual([...nums].sort((a, b) => a - b));
+  const target = [...nums].sort((a, b) => a - b);
+  expect(quickSort2(nums)).toEqual(target);
 });
 
 test('quick sort2 2', () => {
   const nums = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234];
-  expect(quickSort2(nums)).toEqual([...nums].sort((a, b) => a - b));
+  const target = [...nums].sort((a, b) => a - b);
+  expect(quickSort2(nums)).toEqual(target);
 });
