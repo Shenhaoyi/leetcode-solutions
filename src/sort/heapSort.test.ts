6385f1be194fba21540a9a122bc911942a60264d
Shenhaoyi <shenhaoyi04@163.com>
2023-10-04 16:47:41 +0800

    堆排序

--- src/sort/heapSort.test.ts
+++ src/sort/heapSort.test.ts
@@ -1,6 +1,13 @@
-import { heapSort } from './heapSort';
+import { heapSort, heapSort2 } from './heapSort';
 
 test('heap sort', () => {
   const nums = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234];
-  expect(heapSort(nums)).toEqual(nums.sort((a, b) => a - b));
+  const nums1 = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234];
+  expect(heapSort(nums)).toEqual(nums1.sort((a, b) => a - b));
+});
+
+test('heap sort 2', () => {
+  const nums = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234];
+  const nums1 = [2, 42, 64, 234, 62, 12, 8, 6, 5, 3, 2, 21, 5, 6, 6234, 7, 234];
+  expect(heapSort2(nums)).toEqual(nums1.sort((a, b) => a - b));
 });
--- src/sort/heapSort.ts
+++ src/sort/heapSort.ts
@@ -1,5 +1,7 @@
 import { MaxHeap } from '../heap/maxHeap';
+import { swap } from '../utils';
 
+// 利用大顶堆类来实现(推荐)
 export const heapSort = (nums: number[]) => {
   const maxHeap = new MaxHeap(nums);
   const result = [];
@@ -7,4 +9,50 @@ export const heapSort = (nums: number[]) => {
     result.unshift(maxHeap.pop());
   }
   return result;
+};
+
+// 将大顶堆类中用到的内容提取出来，要注意length的获取，nums在pop的过程中长度在变化
+export const heapSort2 = (nums: number[]) => {
+  const getSize = () => nums.length;
+  const getLeft = (i: number) => Math.floor(2 * i + 1);
+  const getRight = (i: number) => Math.floor(2 * i + 2);
+  const siftDown = (i: number) => {
+    if (i >= getSize()) return;
+    const left = getLeft(i);
+    const right = getRight(i);
+    let swapIndex = i; // 记录当前节点和左右子节点中最大值的下标
+    if (left < getSize() && nums[swapIndex] < nums[left]) swapIndex = left;
+    if (right < getSize() && nums[swapIndex] < nums[right]) swapIndex = right;
+    if (swapIndex === i) {
+      return;
+    } else {
+      swap(nums, i, swapIndex);
+      siftDown(swapIndex);
+    }
+  };
+  // 建立大顶堆。O(NlogN)
+  const buildMaxHeap = (nums: number[]) => {
+    const length = getSize();
+    for (let i = length - 1; i >= 0; i--) {
+      siftDown(i);
+    }
+  };
+
+  const pop = () => {
+    swap(nums, 0, getSize() - 1);
+    const target = nums.pop();
+    siftDown(0);
+    return target;
+  };
+
+  const sort = () => {
+    const result: number[] = [];
+    buildMaxHeap(nums);
+    while (getSize()) {
+      result.unshift(pop() as number);
+    }
+    nums.push(...result);
+    return nums;
+  };
+  return sort();
 };
