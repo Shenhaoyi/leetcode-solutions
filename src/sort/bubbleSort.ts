5502399832952c4c9209e3f8e01927f06e742f0a
Shenhaoyi <shenhaoyi04@163.com>
2023-10-09 13:03:49 +0800

    修正

--- src/sort/bubbleSort.ts
+++ src/sort/bubbleSort.ts
@@ -4,7 +4,7 @@ export const bubbleSort = (nums: number[]) => {
   const { length } = nums;
   if (length <= 1) return;
   for (let i = nums.length - 1; i >= 0; i--) {
-    for (let j = 0; j < i - 1; j++) {
+    for (let j = 0; j < i; j++) {
       if (nums[j] > nums[j + 1]) {
         swap(nums, j, j + 1);
       }
