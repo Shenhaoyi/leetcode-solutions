92da6bbd88ab6b34e53cbdb731c94e8a7ffa333b
Shenhaoyi <shenhaoyi04@163.com>
2023-10-12 23:40:46 +0800

    修复算法问题

--- src/sort/insertionSort.ts
+++ src/sort/insertionSort.ts
@@ -2,12 +2,13 @@ import { swap } from '../utils';
 
 export const insertionSort = (nums: number[]) => {
   if (nums.length < 2) return;
-  // 最后一位不需要处理
-  for (let i = 0; i < nums.length - 1; i++) {
+  for (let i = 0; i < nums.length; i++) {
     let pointer = i;
     while (pointer > 0) {
       if (nums[pointer] < nums[pointer - 1]) {
         swap(nums, pointer, pointer - 1);
+      } else {
+        break;
       }
       pointer--;
     }
