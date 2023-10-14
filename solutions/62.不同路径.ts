1151fbebddb366d60b5089206e5470d955edc47d
Shenhaoyi <shenhaoyi04@163.com>
2023-10-14 22:00:27 +0800

    62.不同路径

---
+++ solutions/62.不同路径.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=typescript
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+function uniquePaths(m: number, n: number): number {
+  // dp表
+  const dp = new Array(m).fill(undefined).map(() => new Array(n).fill(0));
+  // 初始状态
+  for (let i = 0; i < n; i++) {
+    dp[0][i] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  // 状态转移
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+    }
+  }
+  return dp[m - 1][n - 1];
+}
+// @lc code=end
