e08353e9625c469d100f9a7602a5b869ef27e561
Shenhaoyi <shenhaoyi04@163.com>
2023-10-19 18:39:18 +0800

    代码优化

--- solutions/322.零钱兑换.ts
+++ solutions/322.零钱兑换.ts
@@ -13,40 +13,29 @@ function coinChange(coins: number[], amount: number): number {
   // 目标是只找到value最小的，且正好达到最大容量
   const { length: m } = coins;
   const n = amount;
+  const MAX = amount + 1;
   const dp = new Array(m + 1).fill(undefined).map(() => new Array(n + 1).fill(0)); // dp[i][j] 面额j所需最少的硬币
+  // 第一列，没有空间，那最小就是0；第一行，从1开始，有空间，但是没有东西能放满，则初始化为一个较大的数字
+  for (let j = 1; j < n + 1; j++) {
+    dp[0][j] = MAX;
+  }
   const currentValue = 1;
   for (let i = 1; i <= m; i++) {
-    // 第一行初始化
-    if (i === 1) {
-      for (let j = 1; j <= n; j++) {
-        const currentWeight = coins[i - 1];
-        if (currentWeight === j) {
-          // 正好装下1个
-          dp[i][j] = currentValue;
-        } else if (j - currentWeight > 0) {
-          dp[i][j] = dp[i][j - currentWeight] + currentValue; // 剩余的空间还能放，但是不一定能刚好放下，所以会出现大于amount+1的情况
-        } else {
-          // 不是正好装下，就代表递归到这里的所有情况都不是正好等于目标面额
-          dp[i][j] = amount + 1;
-        }
-      }
-    } else {
-      for (let j = 1; j <= n; j++) {
-        const currentWeight = coins[i - 1];
-        if (j - currentWeight > 0) {
-          // 可以重复选择！！
-          dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - currentWeight] + currentValue);
-        } else if (j - currentWeight === 0) {
-          // 正好装下1个
-          dp[i][j] = currentValue;
-        } else {
-          // 当前装不下
-          dp[i][j] = dp[i - 1][j];
-        }
+    for (let j = 1; j <= n; j++) {
+      const currentWeight = coins[i - 1];
+      if (j - currentWeight > 0) {
+        // 可以重复选择！！
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - currentWeight] + currentValue);
+      } else if (j - currentWeight === 0) {
+        // 正好装下1个
+        dp[i][j] = currentValue;
+      } else {
+        // 当前装不下
+        dp[i][j] = dp[i - 1][j];
       }
     }
   }
   // 大于amount+1说明所有的情况都不能正好装下
-  return dp[m][n] >= amount + 1 ? -1 : dp[m][n];
+  return dp[m][n] === MAX ? -1 : dp[m][n];
 }
 // @lc code=end
