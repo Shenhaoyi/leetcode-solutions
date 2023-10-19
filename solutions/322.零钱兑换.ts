4f88937849f6f14dec4738e79ab2a3d357010e6b
Shenhaoyi <shenhaoyi04@163.com>
2023-10-19 18:43:02 +0800

    空间优化

--- solutions/322.零钱兑换.ts
+++ solutions/322.零钱兑换.ts
@@ -15,28 +15,28 @@ function coinChange(coins: number[], amount: number): number {
   const { length: m } = coins;
   const n = amount;
   const MAX = amount + 1;
-  const dp = new Array(m + 1).fill(undefined).map(() => new Array(n + 1).fill(0)); // dp[i][j] 面额j所需最少的硬币
+  const dp = new Array(n + 1).fill(0); // dp[j] 面额j所需最少的硬币
   // 第一列，没有空间，那最小就是0；第一行，从1开始，有空间，但是没有东西能放满，则初始化为一个较大的数字
   for (let j = 1; j < n + 1; j++) {
-    dp[0][j] = MAX;
+    dp[j] = MAX;
   }
   const currentValue = 1;
   for (let i = 1; i <= m; i++) {
+    const currentWeight = coins[i - 1];
     for (let j = 1; j <= n; j++) {
-      const currentWeight = coins[i - 1];
       if (j - currentWeight > 0) {
         // 可以重复选择！！
-        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - currentWeight] + currentValue);
+        dp[j] = Math.min(dp[j], dp[j - currentWeight] + currentValue);
       } else if (j - currentWeight === 0) {
         // 正好装下1个
-        dp[i][j] = currentValue;
+        dp[j] = currentValue;
       } else {
         // 当前装不下
-        dp[i][j] = dp[i - 1][j];
+        dp[j] = dp[j];
       }
     }
   }
   // 等于MAX说明所有的情况都不能正好装下
-  return dp[m][n] === MAX ? -1 : dp[m][n];
+  return dp[n] === MAX ? -1 : dp[n];
 }
 // @lc code=end
