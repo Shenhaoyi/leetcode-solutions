57b36b26723beee1363165a232646e1ce28e90d6
Shenhaoyi <shenhaoyi04@163.com>
2023-10-22 02:06:04 +0800

    450.删除二叉搜索树中的节点（迭代法）

---
+++ solutions/450.删除二叉搜索树中的节点.ts
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=typescript
+ *
+ * [450] 删除二叉搜索树中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+// 合并左右子树
+function mergeLeftAndRight(left: TreeNode, right: TreeNode) {
+  let pointer = right;
+  while (pointer.left) {
+    pointer = pointer.left;
+  }
+  pointer.left = left;
+}
+// 查找目标节点
+function findTarget(root: TreeNode, target: number) {
+  let current: TreeNode | null = root;
+  let prev = null; // 当前节点父节点
+  while (current) {
+    if (current.val === target) {
+      break;
+    } else if (current.val < target) {
+      prev = current;
+      current = current.right;
+    } else {
+      prev = current;
+      current = current.left;
+    }
+  }
+  return {
+    prev,
+    current,
+  };
+}
+
+function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
+  // 根据二叉搜索树的性质
+  // 如果目标节点大于当前节点值，则去右子树中删除；
+  // 如果目标节点小于当前节点值，则去左子树中删除；
+  // 如果目标节点就是当前节点，分为以下三种情况：
+  // 其无左子：其右子顶替其位置，删除了该节点；
+  // 其无右子：其左子顶替其位置，删除了该节点；
+  // 其左右子节点都有：其左子树转移到其右子树的最左节点的左子树上，然后右子树顶替其位置，由此删除了该节点。
+  // 链接：https://leetcode.cn/problems/delete-node-in-a-bst/solutions/582561/miao-dong-jiu-wan-shi-liao-by-terry2020-tc0o/
+  if (!root) return root;
+  const { current, prev } = findTarget(root, key);
+  if (!current) return root; // 没找到
+  if (!prev) {
+    // root就是目标节点
+    if (!current!.left || !current!.right) {
+      return current!.left || current!.right;
+    } else {
+      // 找右子树最小的节点，把左子树挂上去
+      mergeLeftAndRight(current!.left, current!.right);
+      return current!.right;
+    }
+  } else {
+    if (!current!.left || !current!.right) {
+      if (prev.left === current) {
+        prev.left = current!.left || current!.right;
+      } else {
+        prev.right = current!.left || current!.right;
+      }
+    } else {
+      // 找右子树最小的节点，把左子树挂上去
+      mergeLeftAndRight(current!.left, current!.right);
+      if (prev.left === current) {
+        prev.left = current!.right;
+      } else {
+        prev.right = current!.right;
+      }
+    }
+    return root;
+  }
+}
+// @lc code=end
