d9aa30bf63b4bc02a9261903c9a61325b89de7c7
Shenhaoyi <shenhaoyi04@163.com>
2023-10-27 20:17:55 +0800

    优化代码

--- solutions/4.寻找两个正序数组的中位数.ts
+++ solutions/4.寻找两个正序数组的中位数.ts
@@ -14,12 +14,13 @@
   递归终点：1.其中一个数组被完全抛弃；2.k减半多次后变成1
 */
 function findKthTargetFromTwoList(nums1: number[], nums2: number[], begin1: number, begin2: number, k: number) {
-  const length1 = nums1.length;
-  const length2 = nums2.length;
-
   // 第1小的，则直接比较
   if (k === 1) return Math.min(nums1[begin1], nums2[begin2]);
 
+  const length1 = nums1.length;
+  const length2 = nums2.length;
+
+  // 确保丢掉的数字少于k/2
   const m = Math.floor(k / 2) - 1; // 2 => 0; 3 => 0; 4 => 1; 5 => 1;  因为2个begin就已经占了2位了，k=2和3的时候都应该是0
   const p1 = Math.min(begin1 + m, length1 - 1); // 越界处理
   const p2 = Math.min(begin2 + m, length2 - 1);
@@ -43,29 +44,36 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
   // 二分查找两个数组中第k大的元素
   const length1 = nums1.length;
   const length2 = nums2.length;
+  // 处理一个数组为空的情况
+  if (!length1) return findMedianSortedArray(nums2);
+  if (!length2) return findMedianSortedArray(nums1);
   const length = length1 + length2;
   let result = 0;
-  // 处理一个数组为空的情况
-  if (!nums1.length || !nums2.length) {
-    const nums = nums1.length ? nums1 : nums2;
-    if (nums.length % 2 === 0) {
-      const m = nums.length / 2;
-      return (nums[m - 1] + nums[m + 1 - 1]) / 2;
-    } else {
-      const m = (nums.length + 1) / 2;
-      return nums[m - 1]; // 下标要-1
-    }
-  }
   if (length % 2 === 0) {
     // 长度和为偶数 0123
     const m = length / 2; // 中位数为m, m+1的平均数
     result =
-      (findKthTargetFromTwoList(nums1, nums2, 0, 0, m) + findKthTargetFromTwoList(nums1, nums2, 0, 0, m + 1)) / 2;
+      (findKthTargetFromTwoList(nums1, nums2, 0, 0, m + 1) + findKthTargetFromTwoList(nums1, nums2, 0, 0, m)) / 2;
   } else {
     // 长度和为奇数 012
-    const m = (length + 1) / 2; // 中位数为m
-    result = findKthTargetFromTwoList(nums1, nums2, 0, 0, m);
+    const m = (length - 1) / 2; // 中位数为m
+    result = findKthTargetFromTwoList(nums1, nums2, 0, 0, m + 1);
   }
   return result;
+}
+
+// 获取有序数组的中位数
+function findMedianSortedArray(nums: number[]) {
+  const { length } = nums;
+  if (!length) return 0;
+  if (length % 2 === 0) {
+    // 0123
+    const m = length / 2;
+    return (nums[m] + nums[m - 1]) / 2;
+  } else {
+    // 012
+    const m = (length - 1) / 2;
+    return nums[m];
+  }
 }
 // @lc code=end
