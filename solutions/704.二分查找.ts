d208e378909cd6a606a6a3d2458829e7ab4cf3da
Shenhaoyi <shenhaoyi04@163.com>
2023-11-13 12:13:03 +0800

    704.二分查找

---
+++ solutions/704.二分查找.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=typescript
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+  let l = 0;
+  let r = nums.length - 1;
+  while (l < r) {
+    const m = Math.floor(l + (r - l) / 2);
+    if (nums[m] < target) l = m + 1;
+    else r = m;
+  }
+  return nums[l] === target ? l : -1;
+}
+// @lc code=end
