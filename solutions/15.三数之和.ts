6c880a4179d8f0f6b99d515a75fce2c11df38a6c
Shenhaoyi <shenhaoyi04@163.com>
2023-11-16 03:06:03 +0800

    优化代码

--- solutions/15.三数之和.ts
+++ solutions/15.三数之和.ts
@@ -14,7 +14,8 @@ function threeSum(nums: number[]): number[][] {
   let result: number[][] = [];
   // 一遍循环 + 双指针, 注意同一个指针不能去相同的数字，否则会有重合的组合
   const { length } = nums;
-  for (let i = 0; i < length; i++) {
+  // -2，因为右边至少还需要 2 个数字
+  for (let i = 0; i < length - 2; i++) {
     if (i > 0 && nums[i] === nums[i - 1]) continue; // 防止重复
     const value1 = nums[i];
     if (value1 > 0) break; // 三个数都超过0则不可能和为0，注意，可以都是0
