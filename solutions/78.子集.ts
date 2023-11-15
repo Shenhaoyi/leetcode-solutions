149e6c631f1d0f8d1fa7037c19155b8b82b4bfd2
Shenhaoyi <shenhaoyi04@163.com>
2023-11-16 00:02:25 +0800

    补充注释

--- solutions/78.子集.ts
+++ solutions/78.子集.ts
@@ -8,6 +8,7 @@
 /* 
   幂集：由该集合全部子集为元素构成的集合
   题解：https://leetcode.cn/problems/subsets/solutions/420458/shou-hua-tu-jie-zi-ji-hui-su-fa-xiang-jie-wei-yun-/
+  思路：就是一颗二叉树的建立，每一层的分叉都是考虑要不要选择当前的元素
   时间复杂度：O(2^N)
   空间复杂度：递归调用栈的代价是 O(N)，state是 O(N)，PowerSet 就可能比较大了
 */
