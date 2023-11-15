bee7d7f0f7ba7aab9803f3a7eeb3a91aec9aae3c
Shenhaoyi <shenhaoyi04@163.com>
2023-11-15 23:02:23 +0800

    补充注释

--- solutions/90.子集-ii.ts
+++ solutions/90.子集-ii.ts
@@ -21,7 +21,7 @@ function subsetsWithDup(nums: number[]): number[][] {
     backTrack(state, index + 1);
     state.pop();
     // 参考 obsidian://advanced-uri?vault=shen&filepath=%25E9%259D%25A2%25E7%25BB%258F%252F%25E7%25AE%2597%25E6%25B3%2595%25E4%25B8%258E%25E6%2595%25B0%25E6%258D%25AE%25E7%25BB%2593%25E6%259E%2584%252F%25E5%2588%25B7%25E9%25A2%2598%25E8%25AE%25B0%25E5%25BD%2595%252F%25E5%259B%259E%25E6%25BA%25AF%252Fduplicated.excalidraw.md
-    // 跳过后面重复的，思路很像 dp 中的一道题 TODO: 看是哪道题
+    // 跳过后面重复的，思路很像【40.组合总和-ii】
     let pointer = index;
     while (nums[pointer] === current) pointer++;
     if (pointer <= length) backTrack(state, pointer);
