3615d1ecc583a5cf9230aebc1775a408be67a8bb
Shenhaoyi <shenhaoyi04@163.com>
2023-11-25 02:51:46 +0800

    另一种方法

--- solutions/40.组合总和-ii.ts
+++ solutions/40.组合总和-ii.ts
@@ -5,6 +5,36 @@
  */
 
 // @lc code=start
+// @ts-ignore
+function backTrack(state: number[], choices: number[], target: number, currentIndex: number, result: number[][]) {
+  if (target === 0) {
+    result.push([...state]);
+  } else if (target > 0) {
+    if (currentIndex > choices.length - 1) return; // 越界（下面的方法在 for 循环中进行限制了）
+    const current = choices[currentIndex];
+    // 当前位置尝试
+    state.push(current);
+    backTrack(state, choices, target - current, currentIndex + 1, result); // 元素不能重复，所以下一次从i+1开始
+    // 当前位置回退
+    state.pop();
+    let pointer = currentIndex + 1;
+    // 当前元素不选
+    while (choices[pointer] === current) pointer++;
+    if (pointer < choices.length) backTrack(state, choices, target, pointer, result);
+  }
+  // 小于就回退
+  // else return
+}
+function combinationSum2(candidates: number[], target: number): number[][] {
+  // 相比39，这里的元素虽然只能选一次，但是会重复出现
+  // 例如数字7 1 7为了避免出现选出1 7和7 1这种重复的情形，将数字先排好序
+  const result: number[][] = [];
+  candidates.sort((a, b) => a - b);
+  backTrack([], candidates, target, 0, result);
+  return result;
+}
+// @lc code=end
+
 function backTrack4(state: number[], choices: number[], target: number, currentIndex: number, result: number[][]) {
   if (target === 0) {
     result.push([...state]);
@@ -22,12 +52,3 @@ function backTrack4(state: number[], choices: number[], target: number, currentI
     }
   }
 }
-function combinationSum2(candidates: number[], target: number): number[][] {
-  // 相比39，这里的元素虽然只能选一次，但是会重复出现
-  // 例如数字7 1 7为了避免出现选出1 7和7 1这种重复的情形，将数字先排好序
-  const result: number[][] = [];
-  candidates.sort((a, b) => a - b);
-  backTrack4([], candidates, target, 0, result);
-  return result;
-}
-// @lc code=end
