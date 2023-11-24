a67da3fb6ba2a934cbfb6bc2aa384887534eca4b
Shenhaoyi <shenhaoyi04@163.com>
2023-11-25 01:09:03 +0800

    补充注释

--- solutions/78.子集.ts
+++ solutions/78.子集.ts
@@ -8,7 +8,7 @@
 /* 
   幂集：由该集合全部子集为元素构成的集合
   题解：https://leetcode.cn/problems/subsets/solutions/420458/shou-hua-tu-jie-zi-ji-hui-su-fa-xiang-jie-wei-yun-/
-  思路：就是一颗二叉树的建立，每一层的分叉都是考虑要不要选择当前的元素
+  思路：就是一颗二叉树的建（先序遍历的顺序），每一层的分叉都是考虑要不要选择当前的元素
   时间复杂度：O(2^N)
   空间复杂度：递归调用栈的代价是 O(N)，state是 O(N)，PowerSet 就可能比较大了
 */
@@ -16,7 +16,7 @@ function subsets(nums: number[]): number[][] {
   const powerSet: number[][] = [];
   const { length } = nums;
   const backTrack = (state: number[], index: number) => {
-    // 刚越界
+    // 刚越界（length - 1的时候还要判断是否选呢！）
     if (index === length) {
       // 遇到完美二叉树的叶子节点时，推入结果中
       powerSet.push([...state]);
