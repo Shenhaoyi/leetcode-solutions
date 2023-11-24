7183914db0af14b4c44b18088db2d16f2c71ca89
Shenhaoyi <shenhaoyi04@163.com>
2023-11-25 02:16:42 +0800

    优化代码

--- solutions/47.全排列-ii.ts
+++ solutions/47.全排列-ii.ts
@@ -5,31 +5,32 @@
  */
 
 // @lc code=start
-function backTrack2(state: number[], choices: number[], used: boolean[], result: number[][]) {
-  if (state.length === choices.length) {
-    result.push([...state]); // 注意要克隆
-  }
-  const set = new Set();
-  for (let i in choices) {
-    const current = choices[i];
-    // 剪枝：1.避免选中已选的元素；2.避免本轮选择的元素重复
-    if (!used[i] && !set.has(current)) {
-      // 试探
-      used[i] = true;
-      state.push(current);
-      backTrack2(state, choices, used, result);
-      // 回退
-      state.pop();
-      used[i] = false;
-      // 避免当前
-      set.add(current);
-    }
-  }
-}
 function permuteUnique(nums: number[]): number[][] {
   const result: number[][] = [];
-  const used = new Array(nums.length).fill(false); // 在对应位置记录元素是否使用过（因为这里有重复，不能使用对象）
-  backTrack2([], nums, used, result);
+  const { length } = nums;
+  const used = Array.from({ length }, () => false); // 在对应位置记录元素是否使用过（因为这里有重复，不能使用对象）
+  const backTrack = (state: number[]) => {
+    if (state.length === length) {
+      result.push([...state]); // 注意要克隆
+    }
+    const set = new Set();
+    for (let i in nums) {
+      const current = nums[i];
+      // 剪枝：1.避免选中已选的元素；2.避免本轮选择的元素重复
+      if (!used[i] && !set.has(current)) {
+        // 试探
+        used[i] = true;
+        state.push(current);
+        backTrack(state);
+        // 回退
+        state.pop();
+        used[i] = false;
+        // 记录本来已选元素，避免当前节点的子节点选择相同元素
+        set.add(current);
+      }
+    }
+  };
+  backTrack([]);
   return result;
 }
 // @lc code=end
