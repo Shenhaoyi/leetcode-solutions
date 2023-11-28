6c6475c20beb8df7a1846628ac54bac2059c3e83
Shenhaoyi <shenhaoyi04@163.com>
2023-11-28 22:05:24 +0800

    543.二叉树的直径

---
+++ solutions/543.二叉树的直径.ts
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=543 lang=typescript
+ *
+ * [543] 二叉树的直径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+  //空或者只有一个节点检测
+  if (!root || (!root.left && !root.right)) return 0;
+  let max = 0;
+  // 求深度的时候，计算以节点为根的直径
+  const maxDepth = (root: TreeNode | null): number => {
+    //空检测
+    if (!root) return 0;
+    const leftDepth = maxDepth(root.left);
+    const rightDepth = maxDepth(root.right);
+    max = Math.max(leftDepth + rightDepth, max); // 不是包含节点数，路径是边数
+    return Math.max(leftDepth, rightDepth) + 1;
+  };
+  maxDepth(root);
+  return max;
+}
+// @lc code=end
