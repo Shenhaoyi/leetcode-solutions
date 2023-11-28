0d0239191817b95181ceed7a47a608a2d95c86d0
Shenhaoyi <shenhaoyi04@163.com>
2023-11-28 18:50:20 +0800

    优化代码

--- solutions/1277.统计全为-1-的正方形子矩阵.ts
+++ solutions/1277.统计全为-1-的正方形子矩阵.ts
@@ -10,9 +10,8 @@ function countSquares(matrix: number[][]): number {
   // 这里是数字，那边是字符串；这里是正方形个数，那边是最大面积
   // 思路：计算每个节点为正方形右下角的正方形数量，进行累加
   const { length: row } = matrix;
-  if (row < 1) return 0;
   const col = matrix[0].length;
-  // 记录以当前节点(i, j)为右下角的正方形数量。（边长多长，个数就有几个）
+  // 记录以当前节点(i, j)为右下角的正方形数量。（边长多长，个数就有几个!!）
   const dp = Array.from({ length: row }, () => new Array<number>(col));
   let result = 0; // 返回的是面积
   for (let i = 0; i < row; i++) {
