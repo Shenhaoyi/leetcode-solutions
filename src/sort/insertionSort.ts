df8ac5f1d5b38f9d35e532ed26f0f7eb6c8e614a
Shenhaoyi <shenhaoyi04@163.com>
2023-12-31 23:12:57 +0800

    优化代码

--- src/sort/insertionSort.ts
+++ src/sort/insertionSort.ts
@@ -1,15 +1,12 @@
 import { swap } from '../utils';
 
 export const insertionSort = (nums: number[]) => {
-  if (nums.length < 2) return;
-  for (let i = 0; i < nums.length; i++) {
+  const { length } = nums;
+  if (length <= 1) return;
+  for (let i = 1; i < length; i++) {
     let pointer = i;
-    while (pointer > 0) {
-      if (nums[pointer] < nums[pointer - 1]) {
-        swap(nums, pointer, pointer - 1);
-      } else {
-        break;
-      }
+    while (pointer > 0 && nums[pointer - 1] > nums[pointer]) {
+      swap(nums, pointer - 1, pointer);
       pointer--;
     }
   }
