db73b1af960ebba6f524f0d0c323188ea9195137
Shenhaoyi <shenhaoyi04@163.com>
2023-12-31 22:59:48 +0800

    优化代码

--- src/sort/bubbleSort.ts
+++ src/sort/bubbleSort.ts
@@ -3,11 +3,9 @@ import { swap } from '../utils';
 export const bubbleSort = (nums: number[]) => {
   const { length } = nums;
   if (length <= 1) return;
-  for (let i = nums.length - 1; i >= 0; i--) {
+  for (let i = length - 1; i >= 0; i--) {
     for (let j = 0; j < i; j++) {
-      if (nums[j] > nums[j + 1]) {
-        swap(nums, j, j + 1);
-      }
+      if (nums[j] > nums[j + 1]) swap(nums, j, j + 1);
     }
   }
 };
