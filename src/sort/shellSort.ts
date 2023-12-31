96e4eea3f33c823a303521921a098475f6aedc28
Shenhaoyi <shenhaoyi04@163.com>
2023-12-31 23:15:56 +0800

    优化代码

--- src/sort/shellSort.ts
+++ src/sort/shellSort.ts
@@ -1,18 +1,17 @@
 import { swap } from '../utils';
 
 export const shellSort = (nums: number[]) => {
-  if (nums.length < 2) return;
-  let gap = Math.floor(nums.length / 2);
+  const { length } = nums;
+  if (length < 2) return;
+  let gap = Math.floor(length / 2);
   while (gap > 0) {
     // 按照间隔进行插入排序
     for (let start = 0; start < gap; start++) {
       // start为当前分隔组的排序起始位置，步长诶为gap
-      for (let i = start; i < nums.length; i += gap) {
+      for (let i = start + gap; i < length; i += gap) {
         let pointer = i;
-        while (pointer > start) {
-          if (nums[pointer] < nums[pointer - gap]) {
-            swap(nums, pointer, pointer - gap);
-          } else break;
+        while (pointer > start && nums[pointer - gap] > nums[pointer]) {
+          swap(nums, pointer, pointer - gap);
           pointer -= gap;
         }
       }
