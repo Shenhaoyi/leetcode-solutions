1ac4f446b8a24263d08305a4160fbc91061aa0fe
Shenhaoyi <shenhaoyi04@163.com>
2023-12-31 23:04:46 +0800

    优化代码

--- src/sort/selectSort.ts
+++ src/sort/selectSort.ts
@@ -1,13 +1,12 @@
 import { swap } from '../utils';
 
 export const selectSort = (nums: number[]) => {
-  if (nums.length <= 1) return;
-  for (let i = nums.length - 1; i >= 0; i--) {
+  const { length } = nums;
+  if (length <= 1) return;
+  for (let i = length - 1; i >= 0; i--) {
     let target = i;
-    for (let j = 0; j < i; j++) {
-      if (nums[j] > nums[target]) {
-        target = j;
-      }
+    for (let j = 0; j <= i /*等于号要加上，因为每个数都要参与比较*/; j++) {
+      if (nums[j] > nums[target]) target = j;
     }
     swap(nums, i, target);
   }
