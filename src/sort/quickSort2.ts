b39a2e2e225a6ab6b183d7f9122170c115e89443
Shenhaoyi <shenhaoyi04@163.com>
2023-12-31 23:21:27 +0800

    优化代码

--- src/sort/quickSort2.ts
+++ src/sort/quickSort2.ts
@@ -3,20 +3,18 @@
  * @param {number} nums
  */
 export const quickSort2 = (nums: number[]): number[] => {
-  if (nums.length <= 1) return nums;
-  const pivot = Math.floor(nums.length / 2);
+  const { length } = nums;
+  if (length <= 1) return nums;
+  const pivot = Math.floor(length / 2);
   const base = nums[pivot];
   const left: number[] = [];
   const right: number[] = [];
   nums.forEach((current, index) => {
     if (index === pivot) return; // 遇到枢纽不处理
-    if (current < base) {
-      left.push(current);
-    } else {
-      // 注意不要用else if> 会漏掉等于的请情况
-      // 注意避免条件重合
-      right.push(current);
-    }
+    if (current < base) left.push(current);
+    // 注意不要用else if> 会漏掉等于的请情况
+    // 注意避免条件重合
+    else right.push(current);
   });
   return [...quickSort2(left), base, ...quickSort2(right)];
 };
