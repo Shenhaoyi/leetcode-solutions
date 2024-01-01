af173f2f0affb02175f7c1889f41755d840a3ce7
Shenhaoyi <shenhaoyi04@163.com>
2024-01-01 14:29:05 +0800

    优化代码

--- solutions/188.买卖股票的最佳时机-iv.ts
+++ solutions/188.买卖股票的最佳时机-iv.ts
@@ -15,27 +15,28 @@ function maxProfit(k: number, prices: number[]): number {
   // 优化一下 k
   k = Math.min(k, Math.floor(length / 2)); // length 最多交易Math.floor(length / 2) 次
   // (天、最多已经交易的次数、是否持有), 0不持有，1 持有
-  const dp = Array.from({ length: k + 1 }, () => new Array<number>(2));
+  const dp = Array.from({ length: k + 1 /*注意这里是 k+1*/ }, () => new Array<number>(2));
   for (let i = 0; i < length; i++) {
-    for (let j = 0; j <= k; j++) {
+    const price = prices[i];
+    for (let j = 0; j < k + 1; j++) {
       if (j === 0) {
         dp[0][1] = -Infinity; // 不可能的情况
         dp[0][0] = 0;
         continue;
       }
       if (i === 0) {
-        dp[j][1] = -prices[0]; // 开始就持有，说明开始就买了
+        dp[j][1] = -price; // 开始就持有，说明开始就买了
         dp[j][0] = 0;
         continue;
       }
       const temp = dp[j][1];
       dp[j][1] = Math.max(
-        dp[j - 1][0] - prices[i], // 当天买入（即持有），买入时 k 加 1
+        dp[j - 1][0] - price, // 当天买入（即持有），买入时 k 加 1
         dp[j][1], // 之前就持有
       );
       dp[j][0] = Math.max(
         dp[j][0], // 延续 i - 1 的未持有
-        temp + prices[i], // i - 1 持有，i 卖出
+        temp + price, // i - 1 持有，i 卖出
       );
     }
   }
@@ -54,24 +55,25 @@ function maxProfit2(k: number, prices: number[]): number {
   // (天、最多已经交易的次数、是否持有), 0不持有，1 持有
   const dp = Array.from({ length }, () => Array.from({ length: k + 1 }, () => new Array<number>(2)));
   for (let i = 0; i < length; i++) {
-    for (let j = 0; j <= k; j++) {
+    const price = prices[i];
+    for (let j = 0; j < k + 1; j++) {
       if (j === 0) {
         dp[i][0][1] = -Infinity; // 不可能的情况
         dp[i][0][0] = 0;
         continue;
       }
       if (i === 0) {
-        dp[0][j][1] = -prices[0]; // 开始就持有，说明开始就买了
+        dp[0][j][1] = -price; // 开始就持有，说明开始就买了
         dp[0][j][0] = 0;
         continue;
       }
       dp[i][j][1] = Math.max(
-        dp[i - 1][j - 1][0] - prices[i], // 当天买入（即持有），买入时 k 加 1
+        dp[i - 1][j - 1][0] - price, // 当天买入（即持有），买入时 k 加 1
         dp[i - 1][j][1], // 之前就持有
       );
       dp[i][j][0] = Math.max(
         dp[i - 1][j][0], // 延续 i - 1 的未持有
-        dp[i - 1][j][1] + prices[i], // i - 1 持有，i 卖出
+        dp[i - 1][j][1] + price, // i - 1 持有，i 卖出
       );
     }
   }
