08dd7be1d1970b7f319d86b89fb2efe0a90ea2dc
Shenhaoyi <shenhaoyi04@163.com>
2024-01-05 15:09:18 +0800

    优化代码

--- solutions/695.岛屿的最大面积.ts
+++ solutions/695.岛屿的最大面积.ts
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-/* 
+/*
   思路-沉岛：2层循环遍历每个点，遇到一个岛屿就去遍历这个岛的所有内容，并且全部变成2，避免重复。
   注意边界的情况
 */
@@ -17,9 +17,7 @@ function maxAreaOfIsland(grid: number[][]): number {
   // 遇到一个为1的点，就开始找到它周围为1的点，全部置为2，也就是让这个岛在后续的遍历中，不再重复遍历。
   const area = (i: number, j: number): number => {
     // 遇到边沿
-    if (i < 0 || i > row - 1 || j < 0 || j > col - 1) {
-      return 0;
-    }
+    if (i < 0 || i > row - 1 || j < 0 || j > col - 1) return 0;
     if (grid[i][j] !== 1) return 0;
     else {
       grid[i][j] = 2; // 剪枝
