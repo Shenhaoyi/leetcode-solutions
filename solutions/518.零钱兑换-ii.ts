05a92d1c4b48afe60352b86c42e05f16d0398376
Shenhaoyi <shenhaoyi04@163.com>
2024-01-10 00:17:08 +0800

    优化注释

--- solutions/518.零钱兑换-ii.ts
+++ solutions/518.零钱兑换-ii.ts
@@ -7,13 +7,13 @@
 // @lc code=start
 // 可重复
 function change(amount: number, coins: number[]): number {
-  // 1、回溯也行，不过回超时，39题的代码可以直接拿来用
+  // 1、回溯也行，不过会超时，39题的代码可以直接拿来用
   // return combinationSum(coins, amount).length
   // 2、dp
   const m = coins.length;
   const n = amount;
   const dp = new Array(n + 1).fill(0); // 第一行表示没有物品能放，初始化为0
-  dp[0] = 1; // 第一列为表示空间为0了，迭代到dp[0]，说明正好前面的空间都被用完了，属于1种组合
+  dp[0] = 1; // 第0列为表示空间为0了，迭代到dp[0]，说明正好前面的空间都被用完了，属于1种组合
   for (let i = 1; i < m + 1; i++) {
     const current = coins[i - 1];
     for (let j = 1; j < n + 1; j++) {
