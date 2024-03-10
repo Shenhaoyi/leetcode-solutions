8ac0a2902c63dd538011a6085b290e1787aa1f43
Shenhaoyi <shenhaoyi04@163.com>
2024-03-10 23:48:17 +0800

    优化

--- solutions/56.合并区间.ts
+++ solutions/56.合并区间.ts
@@ -11,24 +11,21 @@ function merge(intervals: number[][]): number[][] {
     思路：先排序，在遍历一一合并
       如果和右边第一个的没有重合，则与右边的任何一个都没有重合，可以直接 push 到 result 中
   */
-  const result = [];
+  const result: number[][] = [];
   intervals.sort((a, b) => a[0] - b[0]);
 
   let prev = intervals[0];
 
   for (let i = 1; i < intervals.length; i++) {
-    const cur = intervals[i];
-    if (prev[1] >= cur[0]) {
-      // 有重合
-      prev[1] = Math.max(cur[1], prev[1]);
+    const current = intervals[i];
+    if (prev[1] < current[0]) {
+      result.push([...prev]);
+      prev = [...current];
     } else {
-      // 不重合，prev推入res数组
-      result.push(prev);
-      prev = cur; // 更新 prev
+      prev[1] = Math.max(prev[1], current[1]);
     }
   }
-
-  result.push(prev);
+  result.push([...prev]); // 注意，最后一个区间需要单独 push
   return result;
 }
 // @lc code=end
