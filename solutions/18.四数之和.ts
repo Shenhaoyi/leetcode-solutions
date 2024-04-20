85378bd0bb2150c788b9d9edbb62a011ab751bb5
Shenhaoyi <shenhaoyi04@163.com>
2024-04-20 10:59:32 +0800

    优化代码

--- solutions/18.四数之和.ts
+++ solutions/18.四数之和.ts
@@ -13,24 +13,24 @@ function fourSum(nums: number[], target: number): number[][] {
   // -3，因为右边至少还需要 3 个数字
   for (let i = 0; i < length - 3; i++) {
     if (i > 0 && nums[i] === nums[i - 1]) continue;
+    const first = nums[i];
     // -2，因为右边至少还需要 2 个数字
     for (let j = i + 1; j < length - 2; j++) {
       if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+      const second = nums[j];
       let left = j + 1;
       let right = length - 1;
       while (left < right && right < length) {
-        const sum = nums[i] + nums[j] + nums[left] + nums[right];
+        const sum = first + second + nums[left] + nums[right];
         if (sum === target) {
-          result.push([nums[i], nums[j], nums[left], nums[right]]);
+          result.push([first, second, nums[left], nums[right]]);
           while (left < right && nums[right - 1] === nums[right]) right--;
           right--;
           while (left < right && nums[left + 1] === nums[left]) left++;
           left++;
         } else if (sum > target) {
-          while (left < right && nums[right - 1] === nums[right]) right--;
           right--;
         } else {
-          while (left < right && nums[left + 1] === nums[left]) left++;
           left++;
         }
       }
