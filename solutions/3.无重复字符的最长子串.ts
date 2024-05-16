4a5b3fc15474ecb4067cff299f937923478c848b
Shenhaoyi <shenhaoyi04@163.com>
2024-05-16 12:05:11 +0800

    补充注释

--- solutions/3.无重复字符的最长子串.ts
+++ solutions/3.无重复字符的最长子串.ts
@@ -10,8 +10,8 @@ function lengthOfLongestSubstring(s: string): number {
   let result = 0;
   if (s.length < 2) return s.length; // 边界情况
 
-  // 1、双指针（滑动窗口）
-  // 思路：只要重复就将左指针右移，知道把重复的元素排除
+  // 1、双指针（滑动窗口，维护一个没有重复字符的子串）
+  // 思路：只要重复就将左指针右移，直到把重复的元素排除
   // 终止条件，右指针越界
   // 分析：
   // 时间复杂度，O(N)，最多就是2个指针都走到底，走的步骤就算2N
@@ -37,3 +37,23 @@ function lengthOfLongestSubstring(s: string): number {
   return result;
 }
 // @lc code=end
+
+// 错误代码: 没有维护一个没有重复字符的子串，因为 map.get(current) 拿到的下标可能会比 l 小
+/*
+function lengthOfLongestSubstring2(s: string): number {
+    let l = 0;
+    const map = new Map<string, number>(); // 字符-下标
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        const current = s[i];
+        if (map.has(current)) {
+            l = map.get(current)! + 1; // 舍弃相同字符前的内容
+            map.set(current, i)
+        } else {
+            map.set(current, i)
+        }
+        result = Math.max(i - l + 1, result);
+    }
+    return result;
+};
+*/
