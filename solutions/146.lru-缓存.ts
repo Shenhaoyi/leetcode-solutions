154851bf762e2c1bb964f7b467acfe6b09c601f6
Shenhaoyi <shenhaoyi04@163.com>
2024-05-23 01:27:06 +0800

    完善双向链表的解法

--- solutions/146.lru-缓存.ts
+++ solutions/146.lru-缓存.ts
@@ -50,80 +50,59 @@ class LRUCache {
 
 // 参考https://leetcode.cn/problems/lru-cache/solutions/260362/bu-yong-yu-yan-nei-jian-de-map-gua-dang-feng-zhuan/
 // 双向链表才能实现O(1)的删除和添加，比如删除时能拿到前后，直接连上
-
-/* 没有完全通过，就这样吧，知道有这么个东西就行了 */
-class ListNode3<T> {
-  data: T;
-  next: ListNode3<T> | null;
-  prev: ListNode3<T> | null;
-  constructor(data: T) {
-    this.data = data;
+class ListNode3 {
+  value: number;
+  key: number;
+  next: ListNode3 | null;
+  prev: ListNode3 | null;
+  constructor(key: number, value: number) {
+    this.key = key;
+    this.value = value;
     this.next = null;
     this.prev = null;
   }
 }
 
-class DoublyLinkedList2<T> {
-  head: ListNode3<T> | null = null;
-  tail: ListNode3<T> | null = null;
-  isEmpty(): boolean {
-    return this.head === null;
+// 使用 dummy node 简化操作
+class DoublyLinkedList2 {
+  dummyHead: ListNode3;
+  dummyTail: ListNode3;
+
+  constructor() {
+    this.dummyHead = new ListNode3(-1, -1);
+    this.dummyTail = new ListNode3(-1, -1);
+    this.dummyHead.next = this.dummyTail;
+    this.dummyTail.prev = this.dummyHead;
+  }
+  get isEmpty(): boolean {
+    return this.dummyHead.next === this.dummyTail;
   }
   // 插到尾部
-  append(newNode: ListNode3<T>) {
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.prev = this.tail;
-      this.tail!.next = newNode;
-      this.tail = newNode;
-    }
+  append(node: ListNode3) {
+    this.dummyTail.prev!.next = node;
+    node.prev = this.dummyTail.prev;
+    node.next = this.dummyTail;
+    this.dummyTail.prev = node;
   }
   // 删除表头
   shift() {
-    if (!this.head) return;
-    const oldHead = this.head;
-    if (!this.head.next) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.head = this.head.next;
-      if (!this.head.next) {
-        this.tail = this.head;
-      }
-    }
-    this.clearNode(oldHead);
+    if (this.isEmpty) return null;
+    const head = this.dummyHead.next!;
+    this.deleteNode(head!);
+    return head;
   }
-  // 清空节点的链接
-  clearNode(node: ListNode3<T>) {
+  deleteNode(node: ListNode3) {
+    node.prev!.next = node.next;
+    node.next!.prev = node.prev;
     node.prev = null;
     node.next = null;
   }
-  deleteNode(node: ListNode3<T>) {
-    const { prev, next } = node;
-    if (prev) {
-      prev.next = next;
-    }
-    if (this.head === node) {
-      this.head = node.next;
-    }
-    if (this.tail === node) {
-      this.tail = node.prev;
-    }
-    this.clearNode(node);
-  }
-}
-
-interface Data {
-  key: number;
-  value: number;
 }
 
 class LRUCache2 {
   capacity: number;
-  list = new DoublyLinkedList2<Data>();
-  map = new Map<number, ListNode3<Data>>();
+  list = new DoublyLinkedList2();
+  map = new Map<number, ListNode3>();
 
   constructor(capacity: number) {
     this.capacity = capacity;
@@ -139,7 +118,7 @@ class LRUCache2 {
     if (node) {
       this.list.deleteNode(node);
       this.list.append(node);
-      return node.data.value;
+      return node.value;
     } else {
       return -1;
     }
@@ -148,15 +127,14 @@ class LRUCache2 {
   put(key: number, value: number) {
     const node = this.map.get(key);
     if (node) {
-      node.data.value = value;
+      node.value = value;
       this.list.deleteNode(node);
       this.list.append(node);
     } else {
-      const node = new ListNode3({ key, value });
+      const node = new ListNode3(key, value);
       if (this.listSize === this.capacity) {
-        const head = this.list.head;
-        this.map.delete(head!.data.key);
-        this.list.shift();
+        const head = this.list.shift();
+        this.map.delete(head!.key);
       }
       this.list.append(node);
       this.map.set(key, node);
