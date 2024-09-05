44a56beac2c2e308b46a7a9d466516570b43d9ce
Shenhaoyi <shenhaoyi04@163.com>
2024-09-05 14:38:32 +0800

    补充注释

--- solutions/769.最多能完成排序的块.ts
+++ solutions/769.最多能完成排序的块.ts
@@ -11,6 +11,7 @@ function maxChunksToSorted(arr: number[]): number {
     策略
       因为是0 ~ n-1
       当前下标之前的数都小于下标，则当前就是一个分割，在当前元素右侧砍断，区间+1
+      每一个分割都保证 (前一个分割, 这个分割] 之间的数字正好都落在这个区间的下标范围内，这样就能保证区间之间的有序，只要区间内排序就能完成整体的排序
   */
   let result = 0;
   let currentMax = arr[0];
